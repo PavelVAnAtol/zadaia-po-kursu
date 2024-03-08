a34784bba9764c20edea929457fa8a4e0a476fbd
“Pavel_V” <torvokard@gmail.com>
2024-03-08 15:19:35 +0300

    Первая парктика третье задание

---
+++ Z3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+static void CheckIfEven( int number)
+{
+    if (number % 2==0)
+    {
+        Console.WriteLine(" Четное ");
+    }
+    else
+    {
+        Console.WriteLine(" Нечетное ");
+    }
+}
+ Console.WriteLine(" Введите число");
+ int number= Convert.ToInt32(Console.ReadLine());
+ CheckIfEven(number);
