ea879dcfe1c19d177c5466d66e12fb078679cc71
“Pavel_V” <torvokard@gmail.com>
2024-03-08 15:09:37 +0300

    второе  задание

---
+++ Z2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Внутри класса Answer напишите метод FindMax, который принимает на вход три числа и выдаёт максимальное из этих чисел.
+static int FMax(int a, int b, int c)
+{
+    int max = a;
+    if (b > max)
+    {
+        max = b;
+    }
+    if (c > max)
+    {
+        max = c;
+    }
+    return max;
+
+}
+Console.WriteLine("Ведите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите третье  число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = FMax(a, b, c);
+Console.WriteLine($" максимальное число {max} ");
