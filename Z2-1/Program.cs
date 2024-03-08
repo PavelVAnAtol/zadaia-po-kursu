6e0868df6afa7c1bf940a4eb2bb7861d2b6e8fcd
“Pavel_V” <torvokard@gmail.com>
2024-03-08 16:07:23 +0300

    Вторая практика первое задание

---
+++ Z2-1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.WriteLine(" ВВедите Число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0) 
+{
+    Console.WriteLine(" Кратно 23 и 7");
+}
+
+else
+
+{
+    Console.WriteLine(" НЕкратно 23 и 7");
+}
