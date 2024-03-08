1c146d318833dcb86e492de5046c43323a1a64ce
“Pavel_V” <torvokard@gmail.com>
2024-03-08 15:31:38 +0300

    Первая практика четвертое задание

---
+++ Z4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Внутри класса Answer напишите метод PrintEvenNumbers, которая на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), после каждого числа должен быть знак пробела.
+void PrintEvenNumbers(int number)
+{
+    int i = 1;
+    while (i <= number)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($" {i }" );
+        }
+        i++;
+    }
+}
+Console.WriteLine(" Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintEvenNumbers(number);
