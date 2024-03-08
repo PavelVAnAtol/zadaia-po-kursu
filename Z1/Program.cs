2e0ab0493d0a54c1d84eaafb278dbc6f55ac1030
“Pavel_V” <torvokard@gmail.com>
2024-03-08 07:56:13 +0300

    задача один Вывод большего  числа

---
+++ Z1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ведите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите первое число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber==secondNumber)
+{
+    Console.WriteLine("Числа равны");
+}
+if (firstNumber> secondNumber)
+{
+    Console.WriteLine("Первое число больше");
+}
+if (firstNumber< secondNumber)
+
+{
+   Console.WriteLine("Второе число больше") ;
+}
