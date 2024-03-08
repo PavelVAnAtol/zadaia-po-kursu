9b25fd8986aad5f3306437080234a85c7e8232d3
“Pavel_V” <torvokard@gmail.com>
2024-03-08 14:52:07 +0300

    первое задание

--- Z1/obj/Debug/net8.0/Z1.AssemblyInfo.cs
+++ Z1/obj/Debug/net8.0/Z1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Z1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0ab0493d0a54c1d84eaafb278dbc6f55ac1030")]
 [assembly: System.Reflection.AssemblyProductAttribute("Z1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Z1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
